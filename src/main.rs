11b60bd365a3ae18f1827e29cc60f9031123dcf3
toshi_pp <toshiq2@gmail.com>
2018-08-04 19:26:10 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ impl<'a> TSPacket<'a> {
         let transport_error_indicator = bytes[1] & 0x80 > 0;
         let payload_unit_start_indicator = bytes[1] & 0x40 > 0;
         let transport_priority = bytes[1] & 0x20 > 0;
-        let pid = (bytes[1] as u16 & 0x1f << 8) | (bytes[2] as u16);
+        let pid = (u16::from(bytes[1]) & 0x1f << 8) | u16::from(bytes[2]);
         let transport_scrambling_control = bytes[3] >> 6;
         let adaptation_field_control = bytes[3] & 0x30 >> 4;
         let continuity_counter = bytes[3] & 0xf;
@@ -63,7 +63,7 @@ impl AdaptationField {
         if bytes.len() < 1 {
             bail!("too short for adaptation field");
         }
-        let adaptation_field_length = bytes[0] as usize;
+        let adaptation_field_length = usize::from(bytes[0]);
         if bytes.len() < adaptation_field_length + 1 {
             bail!(
                 "adaptation_field_length({}) is bigger than bytes({})",
@@ -90,20 +90,20 @@ struct PESPacket<'a> {
     body: PESPacketBody<'a>,
 }
 
-enum DSMTrickMode {}
 struct Todo {}
-struct PESPacketExtension {
-    pes_private_data: Todo,
-    pack_header: Todo,
+type DSMTrickMode = Todo;
+struct PESPacketExtension<'a> {
+    pes_private_data: Option<&'a [u8]>,
+    pack_header: Option<&'a [u8]>,
     program_packet_sequence_counter: Option<u8>,
     mpeg1_mpeg2_identifier: Option<u8>,
-    original_stuff_length: Todo,
-    p_std_buffer_scale: Todo,
-    p_std_buffer_size: Todo,
+    original_stuff_length: Option<u8>,
+    p_std_buffer_scale: Option<u8>,
+    p_std_buffer_size: Option<u16>,
 }
 
 struct ESCR {
-    base: u32,
+    base: u64,
     extension: u16,
 }
 
@@ -113,22 +113,14 @@ struct NormalPESPacketBody<'a> {
     data_alignment_indicator: u8,
     copyright: u8,
     original_or_copy: u8,
-    pts_dts_flags: u8,
-    escr_flag: u8,
-    es_rate_flag: u8,
-    dsm_trick_mode_flag: u8,
-    additional_copy_info_flag: u8,
-    pes_crc_flag: u8,
-    pes_extension_flag: u8,
-    pes_header_data_length: u8,
-    pts: Option<u32>,
-    dts: Option<u32>,
+    pts: Option<u64>,
+    dts: Option<u64>,
     escr: Option<ESCR>,
     es_rate: Option<u32>,
-    dsm_trick_mode: DSMTrickMode,
-    additional_copy_info: u8,
-    previous_pes_packet_crc: u16,
-    pes_extension: Option<PESPacketExtension>,
+    dsm_trick_mode: Option<DSMTrickMode>,
+    additional_copy_info: Option<u8>,
+    previous_pes_packet_crc: Option<u16>,
+    pes_extension: Option<PESPacketExtension<'a>>,
     pes_packet_data_byte: &'a [u8],
 }
 enum PESPacketBody<'a> {
@@ -143,9 +135,9 @@ impl<'a> PESPacket<'a> {
             bail!("too short for PES packet {}", bytes.len());
         }
         let packet_start_code_prefix =
-            (bytes[0] as u32) << 16 | (bytes[1] as u32) << 8 | (bytes[2] as u32);
+            u32::from(bytes[0]) << 16 | u32::from(bytes[1]) << 8 | u32::from(bytes[2]);
         let stream_id = bytes[3];
-        let pes_packet_length = (bytes[4] as usize) << 8 | (bytes[5] as usize);
+        let pes_packet_length = usize::from(bytes[4]) << 8 | usize::from(bytes[5]);
         let body = match stream_id {
             PROGRAM_STREAM_MAP
             | PRIVATE_STREAM_2
@@ -204,29 +196,167 @@ impl<'a> NormalPESPacketBody<'a> {
         };
         let escr = match escr_flag {
             1 => {
-                if bytes.len() < 6 {
-                    bail!("too short for ESCR");
+                let escr = NormalPESPacketBody::parse_escr(bytes)?;
+                bytes = &bytes[6..];
+                Some(escr)
+            }
+            _ => None,
+        };
+        let es_rate = match es_rate_flag {
+            1 => {
+                let es_rate = u32::from(bytes[0]) & 0x7f << 15
+                    | u32::from(bytes[1]) << 7
+                    | u32::from(bytes[2]) >> 1;
+                bytes = &bytes[3..];
+                Some(es_rate)
+            }
+            _ => None,
+        };
+        let dsm_trick_mode = match dsm_trick_mode_flag {
+            1 => {
+                // todo
+                bytes = &bytes[1..];
+                Some(DSMTrickMode {})
+            }
+            _ => None,
+        };
+        let additional_copy_info = match additional_copy_info_flag {
+            1 => {
+                let additional_copy_info = bytes[0] & 0x7f;
+                bytes = &bytes[1..];
+                Some(additional_copy_info)
+            }
+            _ => None,
+        };
+        let previous_pes_packet_crc = match pes_crc_flag {
+            1 => {
+                let previous_pes_packet_crc = u16::from(bytes[0]) << 8 | u16::from(bytes[1]);
+                bytes = &bytes[2..];
+                Some(previous_pes_packet_crc)
+            }
+            _ => None,
+        };
+        let pes_extension = match pes_extension_flag {
+            1 => {
+                let pes_private_data_flag = bytes[0] & 0x80 > 0;
+                let pack_header_field_flag = bytes[0] & 0x40 > 0;
+                let program_packet_sequence_counter_flag = bytes[0] & 0x20 > 0;
+                let p_std_buffer_flag = bytes[0] & 0x10 > 0;
+                let pes_extension_flag_2 = bytes[0] & 1 > 0;
+                let pes_private_data = match pes_private_data_flag {
+                    true => {
+                        if bytes.len() < 128 {
+                            bail!("too short for PES_private_data");
+                        }
+                        let pes_private_data = &bytes[..128];
+                        bytes = &bytes[129..];
+                        Some(pes_private_data)
+                    }
+                    _ => None,
+                };
+                let pack_header = if pack_header_field_flag {
+                    let pack_field_length = usize::from(bytes[0]);
+                    let pack_header = &bytes[1..pack_field_length + 1];
+                    bytes = &bytes[pack_field_length + 1..];
+                    Some(pack_header)
+                } else {
+                    None
+                };
+                let (
+                    program_packet_sequence_counter,
+                    mpeg1_mpeg2_identifier,
+                    original_stuff_length,
+                ) = match program_packet_sequence_counter_flag {
+                    true => {
+                        let program_packet_sequence_counter = bytes[0] & 0x7f;
+                        let mpeg1_mpeg2_identifier = bytes[1] & 0x40 >> 6;
+                        let original_stuff_length = bytes[1] & 0x3f;
+                        bytes = &bytes[2..];
+                        (
+                            Some(program_packet_sequence_counter),
+                            Some(mpeg1_mpeg2_identifier),
+                            Some(original_stuff_length),
+                        )
+                    }
+                    _ => (None, None, None),
+                };
+                let (p_std_buffer_scale, p_std_buffer_size) = match p_std_buffer_flag {
+                    true => {
+                        let p_std_buffer_scale = bytes[0] & 0x20 >> 5;
+                        let p_std_buffer_size =
+                            u16::from(bytes[0]) & 0x1f << 8 | u16::from(bytes[1]);
+                        bytes = &bytes[2..];
+                        (Some(p_std_buffer_scale), Some(p_std_buffer_size))
+                    }
+                    _ => (None, None),
+                };
+                if pes_extension_flag_2 {
+                    let pes_extension_field_length = usize::from(bytes[0]) & 0x7f;
+                    bytes = &bytes[pes_extension_field_length + 1..];
                 }
-                Some(ESCR {
-                    base: 0,
-                    extension: 0,
+                Some(PESPacketExtension {
+                    pes_private_data,
+                    pack_header,
+                    program_packet_sequence_counter,
+                    mpeg1_mpeg2_identifier,
+                    original_stuff_length,
+                    p_std_buffer_scale,
+                    p_std_buffer_size,
                 })
             }
             _ => None,
         };
+        // stuffing bytes
+        while bytes[0] == 0xff {
+            bytes = &bytes[1..];
+        }
+        let pes_packet_data_byte = bytes;
+        Ok(NormalPESPacketBody {
+            pes_scrambling_control,
+            pes_priority,
+            data_alignment_indicator,
+            copyright,
+            original_or_copy,
+            pts,
+            dts,
+            escr,
+            es_rate,
+            dsm_trick_mode,
+            additional_copy_info,
+            previous_pes_packet_crc,
+            pes_extension,
+            pes_packet_data_byte,
+        })
+    }
 
+    fn parse_optional_fields(bytes: &[u8]) -> Result<PESPacketExtension, Error> {
         unimplemented!();
     }
 
-    fn parse_timestamp(bytes: &[u8]) -> Result<u32, Error> {
+    fn parse_timestamp(bytes: &[u8]) -> Result<u64, Error> {
         if bytes.len() < 5 {
             bail!("too short for timestamp {}", bytes.len());
         }
-        Ok((bytes[0] as u32) >> 1 & 0b111
-            | (bytes[1] as u32)
-            | (bytes[2] as u32) >> 1
-            | (bytes[3] as u32)
-            | (bytes[4] as u32) >> 1)
+        Ok(u64::from(bytes[0]) & 0xe << 29
+            | u64::from(bytes[1]) << 22
+            | u64::from(bytes[2]) & 0xfe << 14
+            | u64::from(bytes[3]) << 7
+            | u64::from(bytes[4]) >> 1)
+    }
+
+    fn parse_escr(bytes: &[u8]) -> Result<ESCR, Error> {
+        if bytes.len() < 6 {
+            bail!("too short for ESCR");
+        }
+        let base = u64::from(bytes[0]) & 0x18 << 27
+            | u64::from(bytes[0]) & 0x3 << 28
+            | u64::from(bytes[1]) << 20
+            | u64::from(bytes[2]) & 0xf8 << 12
+            | u64::from(bytes[2]) & 0x3 << 13
+            | u64::from(bytes[3]) << 5
+            | u64::from(bytes[4]) >> 3;
+        let extension = u16::from(bytes[4]) & 0x3 << 7 | u16::from(bytes[5]) >> 1;
+        Ok(ESCR { base, extension })
     }
 }
 
